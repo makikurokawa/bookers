c628c373ee135ad97645e6a087170d1ca62a2497
makikurokawa <110467242+makikurokawa@users.noreply.github.com>
2022-08-19 13:39:18 +0000

    [途中]20220819

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,12 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id)
+      flash[:notice] = 'Book was successfully created.'
+    else
+      render :index
+    end
   end
 
   def index
@@ -24,8 +28,12 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      redirect_to book_path(book.id)
+      flash[:success] = 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
