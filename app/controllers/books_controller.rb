a143077adf4910c50c489d9bf6bded1a8dbf8d18
makikurokawa <110467242+makikurokawa@users.noreply.github.com>
2022-08-20 13:21:17 +0000

    [完成]20220820

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
       flash[:notice] = 'Book was successfully created.'
     else
+      @books = Book.all
       render :index
     end
   end
@@ -27,11 +28,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
       flash[:success] = 'Book was successfully updated.'
     else
+      @books = Book.find(params[:id])
       render :edit
     end
   end
